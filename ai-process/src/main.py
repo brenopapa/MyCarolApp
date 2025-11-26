f32f937ad8f6096ac39c02c37dfcffa1dd61a80e
Breno Zipoli Monteiro Papa <47231417+brenopapa@users.noreply.github.com>
2025-11-26 06:28:11 -0300

    Update task logging to use HTTP requests
    Refactored task logging to send logs via HTTP POST request.

--- ai-process/src/main.py
+++ ai-process/src/main.py
@@ -1,9 +1,12 @@
-from pycarol import Carol, Tasks
-import os, time
+import os, time, requests
 
-carol = Carol()
-
-task = Tasks(carol).get_task(os.environ["LONGTASKID"])
+task = os.environ["LONGTASKID"]
+token = os.environ["CAROLAPPOAUTH"]
+connector = os.environ["CAROLCONNECTORID"]
+env_domain = os.environ["ENV_DOMAIN"]
+carol_tenant = os.environ["CAROLTENANT"]
+carol_domain = os.environ["CAROLDOMAIN"]
+api_subdomain = os.environ["API_SUBDOMAIN"]
 
 # Tempo total (10 minutos) em segundos
 tempo_total = 60 * 60
@@ -14,7 +17,28 @@ intervalo = 10
 # Loop que executa durante 10 minutos, com intervalo de 10 segundos
 inicio = time.time()
 while time.time() - inicio < tempo_total:
-    task.add_log(log_message=f"TASK EXECUTANDO!!! ... {time.time()}", log_level="INFO")
+    
+    log = [{
+            "mdmTaskId": task,
+            "mdmLogMessage": f"TASK EXECUTANDO!!! ... {time.time()}",
+            "mdmLogLevel": "INFO"
+        }]
+    
+    url = f"https://{api_subdomain}.{carol_domain}/api/v3/tasks/{task}/logs"
+
+    print("Enviando log:", log)
+    print("Para o endpoint:", url)
+
+    requests.post(
+        url,
+        headers={
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-Auth-Key': token,
+            'X-Auth-ConnectorId': connector,
+        },
+        data=log)
+    
     time.sleep(intervalo)  # Espera 10 segundos antes de repetir
 
 
