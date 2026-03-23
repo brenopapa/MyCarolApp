3e1163baeaf861ba969ee8b389084faa7cf41fd1
Breno Zipoli Monteiro Papa <47231417+brenopapa@users.noreply.github.com>
2026-03-23 15:59:17 -0300

    Modify SQL query to include timestamp condition
    Added a timestamp condition to the SQL query.

--- customer.sql
+++ customer.sql
@@ -1,1 +1,2 @@
 SELECT 'a8a201fd4d78325fdf9edcc91834963d' as __mdmId, 0 as __mdmCounterForEntity
+--timestamp-- WHERE mdmCounterForEntity__DATETIME__ > SAFE.DATETIME(TIMESTAMP_MICROS(SAFE_CAST({{start_from}} AS INT64)))
