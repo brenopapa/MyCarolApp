287dcd799a0b8c73fb8910ccae3e8d9e4e05547f
Breno Zipoli Monteiro Papa <47231417+brenopapa@users.noreply.github.com>
2026-03-26 10:17:40 -0300

    Update SQL query to select from ingestion_stg_agent_employee

--- records.sql
+++ records.sql
@@ -1,2 +1,4 @@
 SELECT 'a8a201fd4d78325fdf9edcc91834963d' as __mdmId, 0 as __mdmCounterForEntity
+FROM ingestion_stg_agent_employee
 --timestamp-- WHERE mdmCounterForEntity__DATETIME__ > SAFE.DATETIME(TIMESTAMP_MICROS(SAFE_CAST({{start_from}} AS INT64)))
+LIMIT 1
