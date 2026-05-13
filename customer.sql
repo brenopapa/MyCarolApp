c49f751dbd97c634555d612f5792827405a2f612
Breno Zipoli Monteiro Papa <47231417+brenopapa@users.noreply.github.com>
2026-05-13 13:47:04 -0300

    Refactor SQL MERGE statement for customer ingestion

--- customer.sql
+++ customer.sql
@@ -3,16 +3,16 @@ DECLARE result STRING;
 
   MERGE ingestion_customer AS target
   USING (
-      SELECT 'a8a201fd4d78325fdf9edcc91834963d' as __mdmId, 0 as __mdmCounterForEntity
+      SELECT 'a8a201fd4d78325fdf9edcc91834963d' as mdmId, 0 as mdmCounterForEntity
       FROM ingestion_stg_agent_employee
       --timestamp-- WHERE mdmCounterForEntity__DATETIME__ > SAFE.DATETIME(TIMESTAMP_MICROS(SAFE_CAST({{start_from}} AS INT64)))
       LIMIT 1
   ) AS source
-  ON target.__mdmId = source.__mdmId
+  ON target.mdmId = source.mdmId
   WHEN MATCHED THEN
-      UPDATE SET target.__mdmCounterForEntity = source.__mdmCounterForEntity
+      UPDATE SET target.__mdmCounterForEntity = source.mdmCounterForEntity
   WHEN NOT MATCHED THEN
-      INSERT (__mdmId, __mdmCounterForEntity)
-      VALUES (source.__mdmId, source.__mdmCounterForEntity);
+      INSERT (mdmId, mdmCounterForEntity)
+      VALUES (source.mdmId, source.mdmCounterForEntity);
 
   SELECT result WHERE 1=2;
